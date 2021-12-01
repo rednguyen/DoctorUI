723b5ecca174be8bd66bb646c2dbc93a47654e0c
devisureshh <devisuresh.pyl@gmail.com>
2021-12-01 14:21:14 -0800

    ui changes

--- src/app/fake-data.ts
+++ src/app/fake-data.ts
@@ -6,19 +6,22 @@ export const fakePatients: Patient[] = [{
     address: '123 Main St, Irvine, CA 91223',
     dateofbirth: '08-17-1996',
     email: 'johndoe@gmail.com',
-    phone:'111-111-1111'
+    phone:'111-111-1111',
+    meetingtime: "",
 }, {
     firstname: 'Michael',
     lastname: 'Jackson',
     address: '123 Queen St, New York, NY 964653',
     dateofbirth: '05-15-1983',
     email: 'michaeljackson@gmail.com',
-    phone:'222-222-2222'
+    phone:'222-222-2222',
+    meetingtime: "",
 }, {
     firstname: 'David',
     lastname: 'Ronaldo',
     address: '654 King Ave, New Oakland, UT 66465',
     dateofbirth: '04-12-1956',
     email: 'davidronaldo@gmail.com',
-    phone:'3333-333-3333'
+    phone:'3333-333-3333',
+    meetingtime: "",
 }];
--- src/app/patient/patient.component.ts
+++ src/app/patient/patient.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Patient } from '../types';
 import { PatientsService } from '../patients.service';
-
+import { fakePatients } from '../fake-data';
 
 @Component({
   selector: 'app-patient',
@@ -10,15 +10,16 @@ import { PatientsService } from '../patients.service';
 })
 export class PatientComponent implements OnInit {
 
-  patients: Patient[] = [];
+  // patients: Patient[] = [];
+  patients: Patient[] = fakePatients;
 
   constructor(
     private patientsService: PatientsService
   ) { }
 
   ngOnInit(): void {
-    this.patientsService.getPatients()
-      .subscribe(patients => this.patients = patients)
+    // this.patientsService.getPatients()
+    //   .subscribe(patients => this.patients = patients)
   }
 
 }
